ed96638b5191293f7c37e59146ebbb35b78dafad
Kartik <kartik_a@mt.iitr.ac.in>
2024-08-07 04:03:46 +0530

    added llm models

--- src/app/actions/ai.ts
+++ src/app/actions/ai.ts
@@ -6,10 +6,10 @@ import { gpt4o, gpt4omini } from '@/lib/azure';
 import { getAllMem } from './actions';
 export async function continueConversation(messages: CoreMessage[], model: string) {
     const memories = await getAllMem();
-    console.log(memories)
+    let result;
 
     if (model === 'gpt4o') {
-        const result = await streamText({
+        result = await streamText({
             model: gpt4o('yugaa-gpt4o'),
             system: `Your are a helpful assistant. You are given the previous memories which user has shared with you. You are also given the current conversation. Answer the user's question using only the given memories. If you don't know the answer, just say that you don't know, don't try to make up an answer. 
             
@@ -17,12 +17,9 @@ export async function continueConversation(messages: CoreMessage[], model: strin
             `,
             messages,
         });
-
-        const stream = createStreamableValue(result.textStream);
-        return stream.value;
     }
     if (model === 'gpt4omini') {
-        const result = await streamText({
+        result = await streamText({
             model: gpt4omini('gpt-4o-mini'),
             system: `Your are a helpful assistant. You are given the previous memories which user has shared with you. You are also given the current conversation. Answer the user's question using only the given memories. If you don't know the answer, just say that you don't know, don't try to make up an answer. 
             
@@ -30,9 +27,8 @@ export async function continueConversation(messages: CoreMessage[], model: strin
             `,
             messages,
         });
-
-        const stream = createStreamableValue(result.textStream);
-        return stream.value;
     }
+    const stream = createStreamableValue(result?.textStream);
+    return stream.value;
 
 }
